e46b2f09168e293c170314abb6fce8d80e82be3f
Andrii-dan <89802969+Andrii-dan@users.noreply.github.com>
2022-11-11 00:11:51 +0100

    add task component

--- app/components/AddTask.js
+++ app/components/AddTask.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { Button, Modal, StyleSheet, TextInput, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuid } from 'uuid';
-import { addTask, selectTasks } from '../slices/tasksSlice';
+import {
+	addTask,
+	selectIsAddModalVisible,
+	selectTasks,
+	setIsAddModalVisible,
+} from '../slices/tasksSlice';
 
 const AddTask = () => {
 	const [textInputValue, setTextInputValue] = useState('');
 	const tasksList = useSelector(selectTasks);
+	const isModalVisible = useSelector(selectIsAddModalVisible);
 	const dispatch = useDispatch();
 	const uniqueId = uuid();
 
@@ -17,26 +23,49 @@ const AddTask = () => {
 				task: textInputValue,
 			})
 		);
+		// clearing input
 		setTextInputValue('');
+		// closes add task modal
+		dispatch(setIsAddModalVisible());
 	};
-	console.log(tasksList);
+
+	const cancelHandler = () => {
+		dispatch(setIsAddModalVisible());
+	};
 
 	return (
-		<View className='add-todo'>
-			<TextInput
-				style={styles.textInput}
-				placeholder='Add task'
-				value={textInputValue}
-				onChangeText={(text) => setTextInputValue(text)}
-			/>
-			<Button title='Add Task' onPress={addTaskHandler} />
-		</View>
+		<Modal visible={isModalVisible} animationType='slide'>
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={styles.textInput}
+					placeholder='Add task'
+					value={textInputValue}
+					onChangeText={(text) => setTextInputValue(text)}
+				/>
+				<View style={styles.btnContainer}>
+					<View style={styles.button}>
+						<Button color='#f31282' title='Cancel' onPress={cancelHandler} />
+					</View>
+					<View style={styles.button}>
+						<Button color='#b180f0' title='Add Task' onPress={addTaskHandler} />
+					</View>
+				</View>
+			</View>
+		</Modal>
 	);
 };
 
 export default AddTask;
 
 const styles = StyleSheet.create({
+	inputContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: 16,
+		backgroundColor: '#311b6b',
+		paddingBottom: 250,
+	},
 	textInput: {
 		borderWidth: 1,
 		borderColor: '#e4d0ff',
@@ -46,5 +75,13 @@ const styles = StyleSheet.create({
 		padding: 16,
 		marginBottom: 5,
 		width: '80%',
+	},
+	btnContainer: {
+		marginTop: 16,
+		flexDirection: 'row',
+	},
+	button: {
+		width: 100,
+		marginHorizontal: 8,
 	},
 });
