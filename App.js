265c32f86f69ad270c233f04c55dc5fc08ee2702
Andrii-dan <89802969+Andrii-dan@users.noreply.github.com>
2022-11-11 13:49:48 +0100

    added statusBar styling and app background color

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 const { View, StyleSheet } = require('react-native');
 import { Provider } from 'react-redux';
+import { StatusBar } from 'expo-status-bar';
 import store from './app/store';
 
 import HomeScreen from './app/components/HomeScreen';
@@ -9,6 +10,7 @@ import EditTask from './app/components/EditTask';
 const App = () => {
 	return (
 		<Provider store={store}>
+			<StatusBar style='light'/>
 			<View style={styles.appContainer}>
 				<HomeScreen />
 				<AddTask />
@@ -26,5 +28,6 @@ const styles = StyleSheet.create({
 		paddingTop: 60,
 		paddingBottom: 20,
 		paddingHorizontal: 18,
+		backgroundColor: '#283747',
 	},
 });
